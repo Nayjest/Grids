729ab675b7f0f9cfc82bd1a3adbb5dfeead5b43e
Vitaliy Stepanenko <mail@vitaliy.in>
2014-08-26 17:38:28 +0300

    ArrayDataRow implemented

---
+++ src/Nayjest/Grids/ArrayDataRow.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 27.05.14
+ * Time: 16:18
+ */
+
+namespace Nayjest\Grids;
+
+
+class ArrayDataRow extends DataRow
+{
+
+    protected function extractCellValue(FieldConfig $field)
+    {
+        $field_name = $field->getName();
+        return $this->src[$field_name];
+    }
+} 
