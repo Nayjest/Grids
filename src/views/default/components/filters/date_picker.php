523fa27b549ad83f6bceea7799b7090cc5ddb2b5
Vitaliy Stepanenko <mail@vitaliy.in>
2014-08-28 19:35:38 +0300

    + date_picker filter tpl added

--- src/views/default/components/filters/date_picker.php
+++ src/views/default/components/filters/date_picker.php
@@ -1,12 +1,19 @@
 <?php
 /** @var Nayjest\Grids\Components\Filter $component */
+$id = uniqid();
 ?>
 <?php if($component->getLabel()): ?>
     <span><?= $component->getLabel() ?></span>
 <?php endif ?>
 <input
     class="form-control input-sm"
-    style="display: inline; width: 80px; margin-right: 10px"
+    style="display: inline; width: 85px; margin-right: 10px"
     name="<?= $component->getInputName() ?>"
     value="<?= $component->getValue() ?>"
+    id="<?= $id ?>"
     />
+<script>
+    $(function(){
+        $('#<?= $id ?>').datepicker({format: 'yyyy-mm-dd'});
+    })
+</script>
