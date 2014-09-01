8f8a8c53272930a0d160e53375209b5ad0151470
Vitaliy Stepanenko <mail@vitaliy.in>
2014-09-01 18:37:46 +0300

    default sorting bugfix

--- src/Nayjest/Grids/Sorter.php
+++ src/Nayjest/Grids/Sorter.php
@@ -1,9 +1,6 @@
 <?php
 namespace Nayjest\Grids;
 
-use Form;
-use Request;
-
 class Sorter
 {
     /**
@@ -23,27 +20,35 @@ class Sorter
             ->getUrl();
     }
 
-    protected function sortBy($field, $direction)
-    {
-        foreach ($this->grid->getConfig()->getColumns() as $column) {
-            if ($column->getName() === $field) {
-                $column->setSorting($direction);
-            } else {
-                $column->setSorting(null);
-            }
-        }
-
-        $this->grid->getConfig()->getDataProvider()->orderBy($field, $direction);
-    }
-
     public function apply()
     {
         $input = $this->grid->getInputProcessor()->getInput();
-        //var_dump($input);die();
+        $sort = null;
         if (isset($input['sort'])) {
             foreach ($input['sort'] as $field => $direction) {
-                $this->sortBy($field, $direction);
+                $sort = [$field, $direction];
+                break;
             }
+        }
+        foreach ($this->grid->getConfig()->getColumns() as $column) {
+            if ($sort) {
+                if ($column->getName() === $sort[0]) {
+                    $column->setSorting($sort[1]);
+                } else {
+                    $column->setSorting(null);
+                }
+            } else {
+                if ($direction = $column->getSorting()) {
+                    $sort = [$column->getName(), $direction];
+                }
+            }
+        }
+        if ($sort) {
+            $this
+                ->grid
+                ->getConfig()
+                ->getDataProvider()
+                ->orderBy($sort[0], $sort[1]);
         }
     }
 } 
