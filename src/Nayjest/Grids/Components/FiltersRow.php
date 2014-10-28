72a36c3a2eaa999768b47f46853e4a6afac0990d
Vitaliy Stepanenko <mail@vitaliy.in>
2014-10-28 17:26:59 +0200

    phpdoc

--- src/Nayjest/Grids/Components/FiltersRow.php
+++ src/Nayjest/Grids/Components/FiltersRow.php
@@ -3,6 +3,13 @@ namespace Nayjest\Grids\Components;
 
 use Nayjest\Grids\Components\Base\RenderableRegistry;
 
+/**
+ * Class FiltersRow
+ *
+ * provides additional render sections for columns: 'filters_row_column_<name>'
+ *
+ * @package Nayjest\Grids\Components
+ */
 class FiltersRow extends RenderableRegistry
 {
     protected $template = '*.components.filters_row';
