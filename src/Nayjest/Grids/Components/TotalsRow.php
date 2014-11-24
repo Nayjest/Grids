8a0f1352977279f6a594ee3eb3939b414cd172e4
Vitaliy Stepanenko <mail@vitaliy.in>
2014-11-24 13:11:40 +0200

    TotalsRow component: possibility to specify aggregating function

--- src/Nayjest/Grids/Components/TotalsRow.php
+++ src/Nayjest/Grids/Components/TotalsRow.php
@@ -11,6 +11,7 @@ use Nayjest\Grids\FieldConfig;
 use Nayjest\Grids\IdFieldConfig;
 use Nayjest\Grids\Grid;
 use Illuminate\Support\Facades\Event;
+use Exception;
 
 class TotalsRow extends ArrayDataRow implements IRenderableComponent
 {
@@ -19,10 +20,19 @@ class TotalsRow extends ArrayDataRow implements IRenderableComponent
     }
     use TComponentView;
 
+    const OPERTATION_SUM = 'sum';
+    const OPERATION_AVG = 'avg';
+    //const OPERATION_MAX = 'max';
+    //const OPERATION_MIN = 'min';
+
     /** @var \Illuminate\Support\Collection|FieldConfig[] */
     protected $fields;
 
     protected $field_names;
+
+    protected $field_operations = [];
+
+    protected $rows_processed = 0;
 
     public function __construct(array $field_names = [])
     {
@@ -54,9 +64,24 @@ class TotalsRow extends ArrayDataRow implements IRenderableComponent
             DataProvider::EVENT_FETCH_ROW,
             function (DataRow $row, DataProvider $provider) use ($dp) {
                 if ($dp !== $provider) return;
+                $this->rows_processed++;
                 foreach ($this->fields as $field) {
-                    $this->src[$field->getName()] += $row->getCellValue($field);
+                    $name = $field->getName();
+                    $operation = $this->getFieldOperation($name);
+                    switch($operation) {
+                        case self::OPERTATION_SUM:
+                            $this->src[$name] += $row->getCellValue($field);
+                            break;
+                        case self::OPERATION_AVG:
+                            $this->src["{$name}_sum"] += $row->getCellValue($field);
+                            $this->src[$name] = $this->src["{$name}_sum"] / $this->rows_processed;
+                            break;
+                        default:
+                            throw new Exception("TotalsRow:Unknown aggregation operation.");
+                    }
+
                 }
+
             }
         );
     }
@@ -89,6 +114,55 @@ class TotalsRow extends ArrayDataRow implements IRenderableComponent
         } else {
             return null;
         }
+    }
+
+    /**
+     * @return int
+     */
+    public function getRowsProcessed()
+    {
+        return $this->rows_processed;
+    }
+
+    /**
+     * @param array $field_names
+     */
+    public function setFieldNames($field_names)
+    {
+        $this->field_names = $field_names;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFieldNames()
+    {
+        return $this->field_names;
+    }
+
+    /**
+     * @param array $field_operations
+     */
+    public function setFieldOperations(array $field_operations)
+    {
+        $this->field_operations = $field_operations;
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFieldOperations()
+    {
+        return $this->field_operations;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFieldOperation($field_name)
+    {
+        return isset($this->field_operations[$field_name])?$this->field_operations[$field_name]:self::OPERTATION_SUM;
     }
 
 
--- src/views/default/components/totals.php
+++ src/views/default/components/totals.php
@@ -1,10 +1,27 @@
+<?php
+use Nayjest\Grids\Components\TotalsRow;
+/** @var TotalsRow $component */
+?>
 <tr>
     <?php foreach($columns as $column): ?>
         <td
             class="column-<?= $column->getName() ?>"
             <?= $column->isHidden()?'style="display:none"':'' ?>
             >
-            <?= $component->uses($column)?$column->getValue($component):'' ?>
+            <?php
+            if ($component->uses($column)):
+                $label = '';
+                switch($component->getFieldOperation($column->getName())) {
+                    case \Nayjest\Grids\Components\TotalsRow::OPERTATION_SUM:
+                        $label = '∑';
+                        break;
+                    case \Nayjest\Grids\Components\TotalsRow::OPERATION_AVG:
+                        $label = 'Avg.';
+                        break;
+                }
+                echo $label, '&nbsp;', $column->getValue($component);
+            endif;
+            ?>
         </td>
     <?php endforeach ?>
 </tr>
