51fb0b8d65f961dd146092701d7b9bf6da44d5e2
Vitaliy Stepanenko <mail@vitaliy.in>
2014-12-12 20:56:41 +0200

    Data provider for Doctrine/DBAL experimental implementation

---
+++ src/Nayjest/Grids/DbalDataProvider.php
@@ -0,0 +1,138 @@
+<?php
+namespace Nayjest\Grids;
+
+use DB;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Event;
+use Illuminate\Support\Collection;
+
+class DbalDataProvider extends DataProvider
+{
+
+
+    protected $collection;
+
+    protected $paginator;
+
+    /** @var  $iterator \ArrayIterator */
+    protected $iterator;
+
+    public function __construct(QueryBuilder $src)
+    {
+        $this->src = $src;
+    }
+
+    public function reset()
+    {
+        $this->getIterator()->rewind();
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getCollection()
+    {
+
+        if (!$this->collection) {
+            $query = clone $this->src;
+            $query
+                ->setFirstResult(
+                    ($this->getCurrentPage() - 1) * $this->page_size
+                )
+                ->setMaxResults($this->page_size);
+            $this->collection = Collection::make(
+                DB::select($query, $query->getParameters())
+            );
+        }
+        return $this->collection;
+    }
+
+
+    public function getPaginator()
+    {
+        if (!$this->paginator) {
+            $items = $this->getCollection()->toArray();
+            $this->paginator = \Paginator::make(
+                $items,
+                $this->getTotalRowsCount(),
+                $this->page_size
+            );
+
+        }
+        return $this->paginator;
+    }
+
+    /**
+     * @return \Illuminate\Pagination\Factory
+     */
+    public function getPaginationFactory()
+    {
+        return \App::make('paginator');
+    }
+
+    protected function getIterator()
+    {
+        if (!$this->iterator) {
+            $this->iterator = $this->getCollection()->getIterator();
+        }
+        return $this->iterator;
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    public function getBuilder()
+    {
+        return $this->src;
+    }
+
+    public function getRow()
+    {
+        if ($this->index < $this->getCurrentPageRowsCount()) {
+            $this->index++;
+            $item = $this->iterator->current();
+            $this->iterator->next();
+            $row = new ObjectDataRow($item, $this->getRowId());
+            Event::fire(self::EVENT_FETCH_ROW, [$row, $this]);
+            return $row;
+        } else {
+            return null;
+        }
+    }
+
+    protected $_count;
+
+    /**
+     * @deprecated
+     * @return int
+     */
+    public function count()
+    {
+        return $this->getCurrentPageRowsCount();
+    }
+
+    public function getTotalRowsCount()
+    {
+        return $this->src->execute()->rowCount();
+    }
+
+    public function getCurrentPageRowsCount()
+    {
+        return $this->getCollection()->count();
+    }
+
+    public function orderBy($fieldName, $direction)
+    {
+        $this->src->orderBy($fieldName, $direction);
+        return $this;
+    }
+
+    public function filter($fieldName, $operator, $value)
+    {
+        $this->src->where("$fieldName $operator :$fieldName");
+        $this->src->setParameter($fieldName, $value);
+        return $this;
+    }
+
+} 
