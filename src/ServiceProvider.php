cce36fca3e60a558884e66748b5282fccd61b9b5
Vitaliy Stepanenko <mail@vitaliy.in>
2015-02-17 19:14:34 +0200

    views & translations moved to resources folder, unused migrations folder removed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,7 +6,6 @@ use Route;
 
 class ServiceProvider extends BaseServiceProvider
 {
-
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -30,10 +29,19 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function boot()
     {
+        $pkg_path = dirname(__DIR__);
+        $views_path = $pkg_path . '/resources/views';
 
         # only for Laravel 4 & some of 5-dev
         if (version_compare(Application::VERSION, '5.0.0', '<')) {
             $this->package('nayjest/grids');
+            $this->app['view']->addNamespace('grids', $views_path);
+        } else {
+            $this->loadViewsFrom($views_path, 'grids');
+            $this->loadTranslationsFrom($pkg_path . '/resources/lang', 'grids');
+            $this->publishes([
+                $views_path => base_path('resources/views/nayjest/grids')
+            ]);
         }
     }
 
