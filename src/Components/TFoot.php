13cf83c7560482363faaa26f7ba19a729a86bc98
Vitaliy Stepanenko <mail@vitaliy.in>
2015-02-17 19:50:57 +0200

    fix #25 default paginator class is determined now based on Laravel framework version in TFoot

--- src/Components/TFoot.php
+++ src/Components/TFoot.php
@@ -1,15 +1,22 @@
 <?php
 namespace Nayjest\Grids\Components;
-
+use Illuminate\Foundation\Application;
+use Nayjest\Grids\Components\Pager as L4Pager;
+use Nayjest\Grids\Components\Laravel5\Pager as L5Pager;
 class TFoot extends HtmlTag
 {
     const NAME = 'tfoot';
 
     protected function getDefaultComponents()
     {
+        if (version_compare(Application::VERSION, '5', '<')) {
+            $pagerClass = 'L4Pager';
+        } else {
+            $pagerClass = 'L5Pager';
+        }
         return [
             (new OneCellRow)
-                ->addComponent(new Pager)
+                ->addComponent(new $pagerClass)
         ];
     }
 }
