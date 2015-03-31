b6bf735ef6ed6c43d3aa9b150a82d2ba5f9094a6
Vitaliy Stepanenko <mail@vitaliy.in>
2015-03-31 20:38:17 +0300

    support of 4 default render sections for all renderable registries, refactoring

--- src/Components/Base/RenderableRegistry.php
+++ src/Components/Base/RenderableRegistry.php
@@ -14,5 +14,85 @@ class RenderableRegistry implements
 {
     use TComponent;
     use TRegistry;
-    use TRegistryView;
+    use TComponentView {
+        TComponentView::render as private renderTemplate;
+    }
+
+    const SECTION_BEGIN = 'begin';
+    const SECTION_END = 'end';
+    const SECTION_BEFORE = 'before';
+    const SECTION_AFTER = 'after';
+
+    /**
+     * Renders components related to specified section.
+     *
+     * By default components without specified section will be rendered.
+     *
+     * @param string|null $sectionName
+     * @return string
+     */
+    public function renderComponents($sectionName = null)
+    {
+        $output = '';
+        $components = $this->getSectionComponents($sectionName);
+        foreach ($components as $component) {
+            $output .= $component->render();
+        }
+        return $output;
+    }
+
+    /**
+     * Returns components filtered by section name.
+     *
+     * @param string $sectionName
+     * @return \Illuminate\Support\Collection
+     */
+    public function getSectionComponents($sectionName)
+    {
+        return $this->getComponents()->filter(
+            function (ComponentInterface $component) use ($sectionName) {
+                return $component instanceof RenderableComponentInterface
+                && $component->getRenderSection() === $sectionName;
+            }
+        );
+    }
+
+    /**
+     * Wraps content with outside components (components that have 'before' or 'after' render_section value).
+     *
+     * @param string $output
+     * @return string
+     */
+    protected function wrapWithOutsideComponents($output)
+    {
+        return $this->renderComponents(self::SECTION_BEFORE)
+        . $output
+        . $this->renderComponents(self::SECTION_AFTER);
+    }
+
+    /**
+     * Renders inner components.
+     *
+     * @return string
+     */
+    protected function renderInnerComponents()
+    {
+        return $this->renderComponents(self::SECTION_BEGIN)
+        . $this->renderComponents()
+        . $this->renderComponents(self::SECTION_END);
+    }
+
+    /**
+     * Renders component.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        return $this->wrapWithOutsideComponents(
+            $this->getTemplate()
+                ? $this->renderTemplate()
+                : $this->renderInnerComponents()
+        );
+    }
 }
