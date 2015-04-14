b02b14fa77ebe0f985f5e2e9c09fcac05d2c1993
Vitaliy Stepanenko <mail@vitaliy.in>
2015-04-14 14:00:44 +0300

    Code style fixes

--- resources/views/default/components/csv_export.php
+++ resources/views/default/components/csv_export.php
@@ -2,15 +2,16 @@
 /**
  * @author: Vitaliy Ofat <i@vitaliy-ofat.com>
  *
- * @var $grid Grid
+ * @var $grid Nayjest\Grids\Grid
  */
 use Nayjest\Grids\Components\CsvExport;
-use Nayjest\Grids\Grid;
-
 ?>
 <span>
     <a
-        href="<?= $grid->getInputProcessor()->getUrl([CsvExport::INPUT_PARAM => 1]) ?>"
+        href="<?= $grid
+            ->getInputProcessor()
+            ->getUrl([CsvExport::INPUT_PARAM => 1])
+        ?>"
         class="btn btn-sm btn-default"
         >
         <span class="glyphicon glyphicon-export"></span>
