5380ef1a48d609fcb010268a7eddd379bc0ff540
asaf050 <asafcoheng@gmail.com>
2015-11-22 14:57:21 +0200

    Update FilterConfig.php

--- src/FilterConfig.php
+++ src/FilterConfig.php
@@ -3,7 +3,7 @@ namespace Nayjest\Grids;
 
 class FilterConfig
 {
-	const OPERATOR_LIKE = 'like';
+    const OPERATOR_LIKE = 'like';
     const OPERATOR_EQ = 'eq';
     const OPERATOR_NOT_EQ = 'n_eq';
     const OPERATOR_GT = 'gt';
