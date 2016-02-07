bbc7049eeeef7b25f78c17fce7265cebf211a06c
Andrew Reddin <weranidr@Andrews-iMac.local>
2016-02-07 15:31:51 -0400

    no message

---
+++ tests/CollectionDataProviderTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class CollectionDataProviderTest extends TestCase
+{
+
+    /**
+     * Visit the /project page and verify that a few things are there
+     *
+     * @return void
+     */
+    public function test_visit_index()
+    {
+
+        $this->assertEquals(1,0);
+    }
+
+}
