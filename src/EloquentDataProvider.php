Nayjest/Grids

commit 109f0c4828ccf6cced5e55a445f7a3c514eb0856

Add check Laravel Version before Event:fire (if 5.8 use dispatch instead)

src/EloquentDataProvider.php
@@ -91,7 +91,13 @@ class EloquentDataProvider extends DataProvider
             $item = $this->iterator->current();
             $this->iterator->next();
             $row = new EloquentDataRow($item, $this->getRowId());
-            Event::dispatch(self::EVENT_FETCH_ROW, [$row, $this]);
+
+            if (preg_match('/5.8.*/', App::VERSION())) {
+                Event::dispatch(self::EVENT_PREPARE, $this);
+            } else {
+                Event::fire(self::EVENT_PREPARE, $this);
+            }
+
             return $row;
         } else {
             return null;

src/DbalDataProvider.php
@@ -4,6 +4,7 @@ namespace Nayjest\Grids;
 use DB;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Event;
+use App;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Collection;
 
@@ -96,7 +97,7 @@ class DbalDataProvider extends DataProvider
      */
     public function getPaginationFactory()
     {
-        return \App::make('paginator');
+        return App::make('paginator');
     }
 
     protected function getIterator()
@@ -122,7 +123,13 @@ class DbalDataProvider extends DataProvider
             $item = $this->iterator->current();
             $this->iterator->next();
             $row = new ObjectDataRow($item, $this->getRowId());
-            Event::dispatch(self::EVENT_FETCH_ROW, [$row, $this]);
+
+            if (preg_match('/5.8.*/', App::VERSION())) {
+                Event::dispatch(self::EVENT_FETCH_ROW, [$row, $this]);
+            } else {
+                Event::fire(self::EVENT_FETCH_ROW, [$row, $this]);
+            }
+
             return $row;
         } else {
             return null;

src/Grid.php
@@ -38,7 +38,12 @@ class Grid
         }
 
         $this->initializeComponents();
-        Event::dispatch(self::EVENT_CREATE, $this);
+
+        if (preg_match('/5.8.*/', App::VERSION())) {
+            Event::dispatch(self::EVENT_CREATE, $this);
+        } else {
+            Event::fire(self::EVENT_CREATE, $this);
+        }
     }
 
     /**
@@ -67,7 +72,13 @@ class Grid
         $this->getFiltering()->apply();
         $this->prepareColumns();
         $this->getSorter()->apply();
-        Event::dispatch(self::EVENT_PREPARE, $this);
+
+        if (preg_match('/5.8.*/', App::VERSION())) {
+            Event::dispatch(self::EVENT_PREPARE, $this);
+        } else {
+            Event::fire(self::EVENT_PREPARE, $this);
+        }
+
         $this->prepared = true;
     }
 

commit 564d3e30282e42996a80245732b199e1f0be9de3

Fix bug in EloquentDataProvider.php
I think this bug was introduced by commit : 109f0c4828ccf6cced5e55a445f7a3c514eb0856

src/EloquentDataProvider.php
@@ -93,9 +93,9 @@ class EloquentDataProvider extends DataProvider
             $row = new EloquentDataRow($item, $this->getRowId());
 
             if (version_compare(Application::VERSION, '5.8', '>=')) {
-                Event::dispatch(self::EVENT_PREPARE, $this);
+                Event::dispatch(self::EVENT_FETCH_ROW, $this);
             } else {
-                Event::fire(self::EVENT_PREPARE, $this);
+                Event::fire(self::EVENT_FETCH_ROW, $this);
             }
 
             return $row;